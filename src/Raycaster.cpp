e434f934a9aa146a939224474cf2a57842aadd6b
Connor Mullin <cmullin@ingenio.ca>
2024-07-05 17:37:31 -0600

    comments

--- src/Raycaster.cpp
+++ src/Raycaster.cpp
@@ -14,12 +14,16 @@ Raycaster::~Raycaster (){};
 void
 Raycaster::calculate_pixel_buffer ()
 {
+  // Scan from left side of view to right side, by columns 1 pixel wide
   for (int current_pixel{ 0 }; current_pixel < _pixel_width; current_pixel++)
     {
-      // Calculate ray direction
+      // Calculate ray direction for current pixel
       double camera_planeX = ((double (current_pixel) * 2) / _pixel_width) - 1;
       double ray_directionX = _directionX + (_planeX * camera_planeX);
       double ray_directionY = _directionY + (_planeY * camera_planeX);
       std::cout << "CameraPlaneX: " << camera_planeX << '\n';
+
+      // Use DDA Algorithm (Digital Differential Analyzer) to determine which
+      // points the cast ray needs to check for collisions
     }
 };
