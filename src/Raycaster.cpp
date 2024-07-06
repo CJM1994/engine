cae5a5d6955c2413184e59282b2f562dbd0f46d2
Connor Mullin <cmullin@ingenio.ca>
2024-07-05 19:19:19 -0600

    define vars for DDA algorithm

--- src/Raycaster.cpp
+++ src/Raycaster.cpp
@@ -1,5 +1,5 @@
 #include "../include/Raycaster.h"
-#include <iostream>
+// #include <iostream>
 
 Raycaster::Raycaster (int pixel_width)
     : _positionX (7.0), _positionY (21.0), _directionX (-1.0),
@@ -11,6 +11,12 @@ Raycaster::Raycaster (int pixel_width)
 
 Raycaster::~Raycaster (){};
 
+enum class WallSide
+{
+  HORZ,
+  VERT,
+};
+
 void
 Raycaster::calculate_pixel_buffer ()
 {
@@ -21,9 +27,29 @@ Raycaster::calculate_pixel_buffer ()
       double camera_planeX = ((double (current_pixel) * 2) / _pixel_width) - 1;
       double ray_directionX = _directionX + (_planeX * camera_planeX);
       double ray_directionY = _directionY + (_planeY * camera_planeX);
-      std::cout << "CameraPlaneX: " << camera_planeX << '\n';
+      // std::cout << "camera_planeX: " << camera_planeX << '\n';
 
+      // NOTE:
       // Use DDA Algorithm (Digital Differential Analyzer) to determine which
       // points the cast ray needs to check for collisions
+
+      // Which grid tile of the map we are in
+      int mapX = int (_positionX);
+      int mapY = int (_positionY);
+
+      // Store length of the ray from current position next x side or y side
+      double side_distX;
+      double side_distY;
+
+      // Length of ray from on x/y side to next x/y side
+      double delta_distX;
+      double delta_distY;
+
+      // Which direction to move on x or y axis (+1 or -1)
+      int stepX;
+      int stepY;
+
+      bool hit{ false }; // Was a wall hit
+      WallSide side;     // Was a horizontal wall hit or vertical
     }
 };
