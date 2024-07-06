2bb1026b9deb06f20be5fcb43bb0b7261592651b
Connor Mullin <cmullin@ingenio.ca>
2024-07-05 19:46:48 -0600

    calculate delta distances and initial side distances from starting point

--- src/Raycaster.cpp
+++ src/Raycaster.cpp
@@ -1,5 +1,5 @@
 #include "../include/Raycaster.h"
-// #include <iostream>
+#include <iostream>
 
 Raycaster::Raycaster (int pixel_width)
     : _positionX (7.0), _positionY (21.0), _directionX (-1.0),
@@ -34,16 +34,16 @@ Raycaster::calculate_pixel_buffer ()
       // points the cast ray needs to check for collisions
 
       // Which grid tile of the map we are in
-      int mapX = int (_positionX);
-      int mapY = int (_positionY);
+      int mapX = int (_positionX); // round down
+      int mapY = int (_positionY); // round down
 
       // Store length of the ray from current position next x side or y side
       double side_distX;
       double side_distY;
 
       // Length of ray from on x/y side to next x/y side
-      double delta_distX;
-      double delta_distY;
+      double delta_distX{ std::abs (1 / ray_directionX) };
+      double delta_distY{ std::abs (1 / ray_directionY) };
 
       // Which direction to move on x or y axis (+1 or -1)
       int stepX;
@@ -51,5 +51,30 @@ Raycaster::calculate_pixel_buffer ()
 
       bool hit{ false }; // Was a wall hit
       WallSide side;     // Was a horizontal wall hit or vertical
+
+      // Calculate step direction and the initial side distance
+      if (ray_directionX < 0)
+        {
+          stepX = -1;
+          side_distX = (_positionX - mapX) * delta_distX;
+        }
+      else
+        {
+          stepX = 1;
+          side_distX = (mapX + 1.0 - _positionX) * delta_distX;
+        }
+      if (ray_directionY < 0)
+        {
+          stepY = -1;
+          side_distY = (_positionY - mapY) * delta_distY;
+        }
+      else
+        {
+          stepY = 1;
+          side_distY = (mapY + 1.0 - _positionY) * delta_distY;
+        }
+
+      std::cout << "side_distX: " << side_distX << '\n';
+      std::cout << "side_distY: " << side_distY << '\n';
     }
 };
